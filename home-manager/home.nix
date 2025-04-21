824e1e73e45f23f447bf5f97a6c469f8046a14f6
Begad666 <47504169+Begad666@users.noreply.github.com>
2025-04-21 14:08:02 +0300

    aye aye aye

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -45,7 +45,7 @@
 
   # Add stuff for your user as you see fit:
   # programs.neovim.enable = true;
-  # home.packages = with pkgs; [ steam ];
+  home.packages = with pkgs; [ helvum alsa-utils pulseaudio ];
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
---
+++ pkgs/pipewire-module-xrdp.nix
@@ -0,0 +1,47 @@
+{ stdenv, fetchFromGitHub, lib, pulseaudio, autoreconfHook, pkg-config
+, nixosTests, gitUpdater, }:
+
+stdenv.mkDerivation rec {
+  pname = "pipewire-module-xrdp";
+  version = "0.1";
+
+  src = fetchFromGitHub {
+    owner = "neutrinolabs";
+    repo = pname;
+    rev = "v${version}";
+    hash = "";
+  };
+
+  preConfigure = ''
+    ./bootstrap
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/lib/pipewire/modules $out/libexec/pipewire-xrdp-module $out/etc/xdg/autostart
+    install -m 755 src/.libs/*${stdenv.hostPlatform.extensions.sharedLibrary} $out/lib/pipewire/modules
+
+    install -m 755 instfiles/load_pw_modules.sh $out/libexec/pipewire-xrdp-module/pipewire_xrdp_init
+    substituteInPlace $out/libexec/pipewire-xrdp-module/pipewire \
+      --replace pw-cli ${pipewire}/bin/pwcli
+      --replace pipewire ${pipewire}/bin/pipewire
+      --replace pw-metadata ${pipewire}/bin/pw-metadata
+      --replace pactl ${pipewire}/bin/pactl
+
+    runHook postInstall
+  '';
+
+  nativeBuildInputs = [ autoreconfHook pkg-config pipewire.dev ];
+
+  passthru = { updateScript = gitUpdater { rev-prefix = "v"; }; };
+
+  meta = with lib; {
+    description = "xrdp sink/source pipewire modules";
+    homepage = "https://github.com/neutrinolabs/pipewire-module-xrdp";
+    license = licenses.lgpl21;
+    maintainers = with maintainers; [ lucasew ];
+    platforms = platforms.linux;
+    sourceProvenance = [ sourceTypes.fromSource ];
+  };
+}
