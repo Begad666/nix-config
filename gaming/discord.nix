a14ee55356b59d9e0d17157c6a3e2c187576c30c
Begad666 <47504169+Begad666@users.noreply.github.com>
2025-10-29 13:36:33 +0300

    2nd commit

---
+++ gaming/discord.nix
@@ -0,0 +1,11 @@
+{ config, lib, pkgs, ... }:
+
+{
+  environment.systemPackages = with pkgs;
+    [
+      (discord.override {
+        withVencord = true;
+        withOpenAsar = true;
+      })
+    ];
+}
