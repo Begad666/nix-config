3759ac58959ba340910ec65e2cda1218df8bef20
Begad666 <47504169+Begad666@users.noreply.github.com>
2026-01-18 03:21:19 +0300

    aaa

--- modules/home-manager/desktop/caelestia/default.nix
+++ modules/home-manager/desktop/caelestia/default.nix
@@ -2,7 +2,8 @@
 
 with lib;
 
-{
+let caelestiaConfigDir = ./.;
+in {
   options.modules.desktop.caelestia = {
     enable = mkOption {
       type = types.bool;
@@ -12,6 +13,26 @@ with lib;
   };
 
   config = mkIf config.modules.desktop.caelestia.enable {
-    programs.caelestia-dots.enable = true;
+    programs.caelestia = {
+      enable = true;
+      systemd = {
+        enable = false; # if you prefer starting from your compositor
+        target = "graphical-session.target";
+        environment = [ ];
+      };
+      settings = {
+        bar.status = { showBattery = false; };
+        paths.wallpaperDir = "~/Images";
+      };
+      cli = {
+        enable = true; # Also add caelestia-cli to path
+        settings = { theme.enableGtk = false; };
+      };
+    };
+    # home.packages = with pkgs; [ caelestia-cli ];
+    # xdg.configFile."caelestia/shell.json".source = caelestiaConfigDir
+    #   + "/shell.json";
+    # xdg.stateFile."caelestia/wallpaper/path.txt".source = caelestiaConfigDir
+    #   + "/path.txt";
   };
 }
