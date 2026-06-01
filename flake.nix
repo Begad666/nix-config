d36afe16061b61093f370d74773b91aa9656abd7
Begad666 <47504169+Begad666@users.noreply.github.com>
2026-06-01 19:27:45 +0300

    real stuff?

--- flake.nix
+++ flake.nix
@@ -3,14 +3,14 @@
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     # You can access packages and modules from different nixpkgs revs
     # at the same time. Here's an working example:
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
 
     # Home manager
-    home-manager.url = "github:nix-community/home-manager/release-25.11";
+    home-manager.url = "github:nix-community/home-manager/release-26.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # sops-nix
@@ -84,6 +84,7 @@
           specialArgs = { inherit inputs outputs; };
           modules = outputs.allNixosModules ++ [
             inputs.nixos-wsl.nixosModules.default
+            inputs.impermanence.nixosModules.impermanence
             inputs.sops-nix.nixosModules.sops
             # > Our main nixos configuration file <
             ./hosts/beliku-wsl/configuration.nix
@@ -93,11 +94,23 @@
           system = "x86_64-linux";
           specialArgs = { inherit inputs outputs; };
           modules = outputs.allNixosModules ++ [
+            inputs.impermanence.nixosModules.impermanence
             inputs.sops-nix.nixosModules.sops
             # > Our main nixos configuration file <
             ./hosts/beliku-vm/configuration.nix
           ];
         };
+		wg-vps = nixpkgs.lib.nixosSystem {
+		  system = "x86_64-linux";
+		  specialArgs = { inherit inputs outputs; };
+		  modules = outputs.allNixosModules ++ [
+			"${nixpkgs}/nixos/modules/virtualisation/digital-ocean-image.nix"
+            inputs.impermanence.nixosModules.impermanence
+			inputs.sops-nix.nixosModules.sops
+			# > Our main nixos configuration file <
+			./hosts/wg-vps/configuration.nix
+		  ];
+		};
       };
 
       # Standalone home-manager configuration entrypoint
